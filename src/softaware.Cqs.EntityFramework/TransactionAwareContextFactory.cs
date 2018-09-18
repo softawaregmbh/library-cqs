1c57f53d6daae2e970a5c400bf1250b6d0f47d9d
Michael Einsiedler <einsiedler.michael@gmail.com>
2018-09-18 20:19:11 +0200

    add TransactionAwareContextFactory in separate project

---
+++ src/softaware.Cqs.EntityFramework/TransactionAwareContextFactory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Transactions;
+using Microsoft.EntityFrameworkCore;
+
+namespace softaware.Cqs.EntityFramework
+{
+    public static class TransactionAwareContextFactory
+    {
+        private static ConcurrentDictionary<Transaction, DbConnection> connections = new ConcurrentDictionary<Transaction, DbConnection>();
+
+        public static TContext CreateContext<TContext>(
+            string connectionString,
+            Func<DbContextOptions<TContext>, TContext> dbContextFuncWithOptions,
+            Func<DbConnection, TContext> dbContextFuncWithDbConnection)
+            where TContext : DbContext
+        {
+            var currentTransaction = Transaction.Current;
+            if (currentTransaction == null)
+            {
+                var options = new DbContextOptionsBuilder<TContext>()
+                    .UseSqlServer(connectionString)
+                    .Options;
+
+                return dbContextFuncWithOptions(options);
+            }
+            else
+            {
+                var connectionForTransaction = connections.GetOrAdd(currentTransaction, valueFactory: t =>
+                {
+                    var connection = new SqlConnection(connectionString);
+                    connection.Open();
+
+                    t.TransactionCompleted += (s, e) =>
+                    {
+                        connection.Close();
+                        connections.TryRemove(t, out var _);
+                    };
+
+                    return connection;
+                });
+
+                return dbContextFuncWithDbConnection(connectionForTransaction);
+            }
+        }
+    }
+}
