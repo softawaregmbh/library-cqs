cb09aa869612534f2c134dde1a5462acaec6b194
Michael Einsiedler <michael.einsiedler@softaware.at>
2018-09-24 11:52:24 +0200

    add UsageAware command/query decorators

---
+++ src/softaware.Cqs.Decorators.UsageAware/UsageAwareCommandHandlerDecorator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace softaware.Cqs.Decorators.UsageAware
+{
+    public class UsageAwareCommandHandlerDecorator<TCommand> : ICommandHandler<TCommand>
+        where TCommand : ICommand
+    {
+        private readonly UsageAwareCommandLogger<TCommand> logger;
+        private readonly ICommandHandler<TCommand> decoratee;
+
+        public UsageAwareCommandHandlerDecorator(
+            UsageAwareCommandLogger<TCommand> logger,
+            ICommandHandler<TCommand> decoratee)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.decoratee = decoratee ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task HandleAsync(TCommand command)
+        {
+            await this.logger.TimeAndLogCommandAsync(() => this.decoratee.HandleAsync(command));
+        }
+    }
+}
---
+++ src/softaware.Cqs.Decorators.UsageAware/UsageAwareQueryHandlerDecorator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace softaware.Cqs.Decorators.UsageAware
+{
+    public class UsageAwareQueryHandlerDecorator<TQuery, TResult> : IQueryHandler<TQuery, TResult>
+        where TQuery : IQuery<TResult>
+    {
+        private readonly UsageAwareQueryLogger<TQuery, TResult> logger;
+        private readonly IQueryHandler<TQuery, TResult> decoratee;
+
+        public UsageAwareQueryHandlerDecorator(
+            UsageAwareQueryLogger<TQuery, TResult> logger,
+            IQueryHandler<TQuery, TResult> decoratee)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.decoratee = decoratee ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public Task<TResult> HandleAsync(TQuery query)
+        {
+            return this.logger.TimeAndLogQueryAsync(() => this.decoratee.HandleAsync(query));
+        }
+    }
+
+}
